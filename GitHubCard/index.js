66052f6a33dd8a4d0c590ee37939c3d57929bd50
Tony Lu <TonyLu@Tonys-iMac.fios-router.home>
2021-07-16 00:13:51 -0400

    Completed steps 1 through 4

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,18 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/tlu8592')
+  .then(response => {
+    const userDataObj = response.data;
+    // Step 4
+    const cardsDiv = document.querySelector('.cards');
+    cardsDiv.appendChild(cardMaker(userDataObj));
+  })
+  .catch(error => console.log(error));
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -24,8 +34,10 @@
     bottom of the page. Get at least 5 different Github usernames and add them as
     Individual strings to the friendsArray below.
 
-    Using that array, iterate over it, requesting data for each user, creating a new card for each
-    user, and adding that card to the DOM.
+    Using that array, iterate over it, requesting data for each userLocation.textContent = `Location: ${obj.location}`;, creating a new card for each
+    
+    userLocation.textContent = `Location: ${obj.location}`;, and adding that card to the DOM.
+    
 */
 
 const followersArray = [];
@@ -35,10 +47,12 @@ const followersArray = [];
     Using DOM methods and properties, create and return the following markup:
 
     <div class="card">
-      <img src={image url of user} />
+      <img src={image url of  userLocation.textContent = `Location: ${obj.location}`;} />
+      
       <div class="card-info">
         <h3 class="name">{users name}</h3>
-        <p class="username">{users user name}</p>
+        <p class="username">{users  userLocation.textContent = `Location: ${obj.location}`; name}</p>
+        
         <p>Location: {users location}</p>
         <p>Profile:
           <a href={address to users github page}>{address to users github page}</a>
@@ -49,6 +63,55 @@ const followersArray = [];
       </div>
     </div>
 */
+
+function cardMaker(userObj) {
+  const cardDiv = document.createElement('div');
+  cardDiv.classList.add('card');
+
+  const userImg = document.createElement('img');
+  userImg.setAttribute('src', `${userObj.avatar_url}`);
+
+  const cardInfoDiv = document.createElement('div');
+  cardInfoDiv.classList.add('card-info');
+
+  const name = document.createElement('h3');
+  name.classList.add('name')
+  name.textContent = `${userObj.name}`;
+
+  const userName = document.createElement('p');
+  userName.classList.add('username');
+  userName.textContent = `${userObj.login}`;
+
+  const userLocation = document.createElement('p');
+  userLocation.textContent = `Location: ${userObj.location}`;
+
+  const userProfileLink = document.createElement('a');
+  userProfileLink.setAttribute('href', `${userObj.html_url}`);
+
+  const userProfile = document.createElement('p');
+  userProfile.textContent = `Profile: ${userProfileLink}`;
+
+  const followersCount = document.createElement('p');
+  followersCount.textContent = `Followers: ${userObj.followers}`;
+
+  const followingCount = document.createElement('p');
+  followingCount.textContent = `Following: ${userObj.following}`;
+
+  const userBio = document.createElement('p');
+  userBio.textContent = `Bio: ${userObj.bio}`;
+
+  cardDiv.appendChild(userImg);
+  cardDiv.appendChild(cardInfoDiv);
+  cardInfoDiv.appendChild(name);
+  cardInfoDiv.appendChild(userName);
+  cardInfoDiv.appendChild(userLocation);
+  cardInfoDiv.appendChild(userProfile);
+  cardInfoDiv.appendChild(followersCount);
+  cardInfoDiv.appendChild(followingCount);
+  cardInfoDiv.appendChild(userBio);
+
+  return cardDiv;
+}
 
 /*
   List of LS Instructors Github username's:
